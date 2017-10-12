c2bd0e2e04cbc5446cd5b503bfb681a8a38a119b
Milena Garcés <milenagarces@Milenas-MacBook-Pro.local>
2017-10-12 19:53:51 -0400

    troubleshooting

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var basicQ = require("./basic.js")
 var resolvedQ = [];
 
 for (var i = 0; i < basicQ.length; i++) {
+
 	var q = new basicCard (basicQ[i].front, basicQ[i].back);
 	resolvedQ.push(q);
 }
@@ -20,11 +21,12 @@ var answerWrong = 0;
 
 
 
-function askQuestion() {
+function askQuestion(q) {
+
     inquirer.prompt ([{
         type: 'input',
         // If I call q.front[i] there is not question prompted in the terminal// q.front is prompting the same question
-        message: q.front,
+        message: resolvedQ[q].front,
         name: 'userGuess'
     }
 
@@ -39,8 +41,8 @@ function askQuestion() {
     }
 	// Go to the next question
     if (q< resolvedQ.length - 1) {
-        q++;
-        askQuestion();
+        
+        askQuestion(q++);
     } else {
         console.log('Game Over!');
         console.log('Correct Answers: ' + answerRight);
@@ -61,7 +63,7 @@ function askQuestion() {
     
                 // Ask the questions again
                 //Is not working when the user wants to play again
-                askQuestion();
+                askQuestion(q);
             } else {
                 // Exit the game
                 console.log('Thanks for playing Flashcards!');
@@ -71,4 +73,4 @@ function askQuestion() {
 })
 }
 
-askQuestion();
+askQuestion(0);
